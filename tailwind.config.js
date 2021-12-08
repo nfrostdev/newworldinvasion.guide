826768d3a13c6a32e312a623e55ca8b3095174eb
Nick Frost <nfrost.dev@gmail.com>
2021-12-08 14:10:09 -0500

    work on colors and header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                'nw-lightgray': '#d5d5d5',
+                'nw-gray': '#272624',
+                'nw-darkgray': '#1a1918',
+                'nw-orange': '#ffb10f',
+                'nw-gold': '#fff1ce'
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
