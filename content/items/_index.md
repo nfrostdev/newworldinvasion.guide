a5bdf2e0c59d139ba586263e451c628cd9d8be43
Nick Frost <nfrost.dev@gmail.com>
2021-12-10 10:01:13 -0500

    work out some build kinks, remove items link

--- content/items/_index.md
+++
@@ -1,8 +0,0 @@
----
-title: "Items"
-date: 2021-12-09T10:49:09-05:00
-draft: false
----
-
-# Items
-Work in progress.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-    purge: [],
+    purge: {
+        enabled: true,
+        content: [
+            "./layouts/**/*.html",
+            "./content/**/*.md",
+            "./content/**/*.html",
+            "./assets/**/*.js",
+        ]
+    },
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
