03d249ab728a72d0e57b798e8dddffaec510e6cc
Nick Frost <nfrost.dev@gmail.com>
2021-12-16 10:17:15 -0500

    close header nav on scroll, zindex fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,8 @@
 document.querySelector('.header__toggle').addEventListener('click', function () {
     document.querySelector('.header__nav').classList.toggle('shown')
 })
+
+window.addEventListener('scroll', function () {
+        document.querySelector('.header__nav').classList.remove('shown')
+    }
+)
